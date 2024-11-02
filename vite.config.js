11e81f13c5952b6899efb3af06bc1e44cdc3fed9
IasonTsivos <iaswnas1o@gmail.com>
2024-11-02 21:04:57 +0200

    add base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Update base path
+  base: '/IasonTsivos.github.io/', // Update base path
 })
